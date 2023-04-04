24ae2a7ab76100b28a5518dc9544581ac5b8ec4a
Samnang MA <masamnang6@gmail.com>
2023-04-04 10:08:49 +0700

    Function-arrow funtions - DOM

---
+++ Function/table.js
@@ -0,0 +1,201 @@
+let categories = [
+    {
+      id: 1,
+      name: "Clotheaaa",
+      image: "https://api.lorem.space/image/fashion?w=640&h=480&r=1004",
+      creationAt: "2023-04-03T09:42:01.000Z",
+      updatedAt: "2023-04-03T12:28:55.000Z",
+    },
+    {
+      id: 2,
+      name: "Electronics",
+      image: "https://api.lorem.space/image/watch?w=640&h=480&r=3876",
+      creationAt: "2023-04-03T09:42:01.000Z",
+      updatedAt: "2023-04-03T09:42:01.000Z",
+    },
+    {
+      id: 3,
+      name: "Furniture",
+      image: "https://api.lorem.space/image/furniture?w=640&h=480&r=7370",
+      creationAt: "2023-04-03T09:42:01.000Z",
+      updatedAt: "2023-04-03T09:42:01.000Z",
+    },
+    {
+      id: 4,
+      name: "Shoes",
+      image: "https://api.lorem.space/image/shoes?w=640&h=480&r=1088",
+      creationAt: "2023-04-03T09:42:01.000Z",
+      updatedAt: "2023-04-03T09:42:01.000Z",
+    },
+    {
+      id: 5,
+      name: "Others",
+      image: "https://api.lorem.space/image?w=640&h=480&r=2437",
+      creationAt: "2023-04-03T09:42:01.000Z",
+      updatedAt: "2023-04-03T09:42:01.000Z",
+    },
+    {
+      id: 7,
+      name: "asdfadf",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T12:38:37.000Z",
+      updatedAt: "2023-04-03T12:38:37.000Z",
+    },
+    {
+      id: 9,
+      name: "nueva asdfazxc",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/platzi-store-forms.appspot.com/o/category.png?alt=media&token=75efd6b3-b4c5-446d-83bc-0344ec41d6ad",
+      creationAt: "2023-04-03T14:01:02.000Z",
+      updatedAt: "2023-04-03T14:01:02.000Z",
+    },
+    {
+      id: 10,
+      name: "Foto nueva",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T15:40:02.000Z",
+      updatedAt: "2023-04-03T15:40:02.000Z",
+    },
+    {
+      id: 11,
+      name: "Foto nueva",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T15:52:58.000Z",
+      updatedAt: "2023-04-03T15:52:58.000Z",
+    },
+    {
+      id: 13,
+      name: "Foto nueva",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T16:28:35.000Z",
+      updatedAt: "2023-04-03T16:28:35.000Z",
+    },
+    {
+      id: 15,
+      name: "Foto nueva",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T16:57:53.000Z",
+      updatedAt: "2023-04-03T16:57:53.000Z",
+    },
+    {
+      id: 16,
+      name: "Foto nueva",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T17:06:27.000Z",
+      updatedAt: "2023-04-03T17:06:27.000Z",
+    },
+    {
+      id: 17,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T17:27:40.000Z",
+      updatedAt: "2023-04-03T17:27:40.000Z",
+    },
+    {
+      id: 18,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T17:39:58.000Z",
+      updatedAt: "2023-04-03T17:39:58.000Z",
+    },
+    {
+      id: 19,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T18:02:05.000Z",
+      updatedAt: "2023-04-03T18:02:05.000Z",
+    },
+    {
+      id: 20,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T18:06:38.000Z",
+      updatedAt: "2023-04-03T18:06:38.000Z",
+    },
+    {
+      id: 21,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T18:10:16.000Z",
+      updatedAt: "2023-04-03T18:10:16.000Z",
+    },
+    {
+      id: 22,
+      name: "un nuevo nombre",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T21:25:59.000Z",
+      updatedAt: "2023-04-03T21:35:21.000Z",
+    },
+    {
+      id: 23,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-03T21:41:47.000Z",
+      updatedAt: "2023-04-03T21:41:47.000Z",
+    },
+    {
+      id: 24,
+      name: "nuevo2",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/platzi-store-forms.appspot.com/o/category.png?alt=media&token=1c8f03f5-d35b-4a3a-904d-373169aa4111",
+      creationAt: "2023-04-03T22:52:42.000Z",
+      updatedAt: "2023-04-03T22:52:42.000Z",
+    },
+    {
+      id: 25,
+      name: "Gatos",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-04T00:29:24.000Z",
+      updatedAt: "2023-04-04T00:29:24.000Z",
+    },
+    {
+      id: 26,
+      name: "assasd",
+      image: "http://platzi.com:4200/admin/categories/create",
+      creationAt: "2023-04-04T00:33:47.000Z",
+      updatedAt: "2023-04-04T00:33:47.000Z",
+    },
+    {
+      id: 27,
+      name: "New Category",
+      image: "https://placeimg.com/640/480/any",
+      creationAt: "2023-04-04T00:41:56.000Z",
+      updatedAt: "2023-04-04T00:41:56.000Z",
+    },
+    {
+      id: 28,
+      name: "asdasdasdas",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/platzi-store-forms.appspot.com/o/category.png?alt=media&token=7aa67dba-124a-4bc3-864e-c641e5d36896",
+      creationAt: "2023-04-04T01:02:27.000Z",
+      updatedAt: "2023-04-04T01:02:27.000Z",
+    },
+    {
+      id: 29,
+      name: "asdasdas",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/platzi-store-forms.appspot.com/o/category.png?alt=media&token=7f5b8a89-d745-4879-83f7-2970e32b7178",
+      creationAt: "2023-04-04T01:38:22.000Z",
+      updatedAt: "2023-04-04T01:38:22.000Z",
+    },
+  ];
+  
+  let tbody = document.querySelector("tbody");
+  
+  categories.map((category) => {
+    console.log(category);
+    tbody.innerHTML += ` 
+    <table class="table">
+    <tbody>
+      <tr>
+        <th scope="row">${category.id}</th>
+        <td>${category.name}</td>
+        <td><img src="${category.image}" width="200"></td>
+      </tr>
+
+    </tbody>
+  </table>`;
+  });
+
+
+
